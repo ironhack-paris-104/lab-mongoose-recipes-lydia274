cf3748074be188e73e8f45c129986e5fe13cf389
lydia a <lydie.antonova@gmail.com>
2023-05-22 18:28:14 +0200

    done for today

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose")
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model")
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data")
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://127.0.0.1:27017/recipe-app"
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
+  .then((mongoose) => {
+    console.log(`Connected to the database: "${mongoose.connection.name}"`)
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Pizza",
+      level: "Easy Peasy",
+      ingredients: "dough",
+      cuisine: "italian",
+      dishType: "main_course",
+      duration: 15,
+      creator: "moi",
+    })
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  .catch((error) => {
+    console.error("Error connecting to the database", error)
+  })
